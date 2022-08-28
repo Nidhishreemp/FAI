10b8294afd01481d10ee5c2dc3c9bc7a55199a05
Nidhishreemp <71564630+Nidhishreemp@users.noreply.github.com>
2022-08-28 22:21:36 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iofileassigmnet
+{
+    //this serializable class models product data that includes product id, product name, price and in-stock status
+    public class Product
+    {
+        /*
+         * define properties for Product model attributes 
+         */
+        public int ProductId;
+        public string ProductName;
+        public int price;
+        public bool InStock;
+
+        
+
+
+
+        /*
+         * Override ToString() method to return string equivalent of product object containing product details
+         */
+    }
+}
